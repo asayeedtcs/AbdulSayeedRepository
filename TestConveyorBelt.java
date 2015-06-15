c2c2b3f8d75224f065e1d1ee23acc9402657a8d1
asayeedtcs <asayeedtcs@gmail.com>
2015-06-15 12:26:46 +0530

    Test class

---
+++ TestConveyorBelt.java
@@ -0,0 +1,16 @@
+package test.barclays.baggage.conveyorsystem.engine;
+
+import org.junit.Test;
+
+import com.barclays.baggage.conveyorsystem.CoveyorBelt;
+
+public class TestConveyorBelt {
+
+	@Test
+	public void executeTest() {
+
+		CoveyorBelt.main(null);
+
+	}
+
+}
